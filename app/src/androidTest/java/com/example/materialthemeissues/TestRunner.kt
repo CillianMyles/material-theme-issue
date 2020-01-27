93a1263ba61b83c029c7a573de7ae8cae54d8910
Cillian Myles <mylsey4thewin@gmail.com>
2020-01-27 15:08:54 +0000

    Merge branch 'master' into branch 'support'

---
+++ app/src/androidTest/java/com/example/materialthemeissues/TestRunner.kt
@@ -0,0 +1,10 @@
+package com.example.materialthemeissues
+
+import android.support.test.runner.AndroidJUnitRunner
+
+/**
+ * Created by Cillian Myles on 27/01/2020.
+ * Copyright (c) 2020 Cillian Myles. All rights reserved.
+ */
+
+class TestRunner : AndroidJUnitRunner()
