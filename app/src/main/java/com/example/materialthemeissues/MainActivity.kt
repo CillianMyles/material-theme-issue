996b2829b0288450f2df5a974151e455f9d084a1
Cillian Myles <mylsey4thewin@gmail.com>
2020-01-29 16:25:43 +0000

    Fixing app name and main activity title;

--- app/src/main/java/com/example/materialthemeissues/MainActivity.kt
+++ app/src/main/java/com/example/materialthemeissues/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTitle(R.string.android_x_name)
         setContentView(R.layout.activity_main)
     }
 
